80dfefa1c29b92db3ee4af4e33e8de6847042730
MadisonC-SparkFun <madison.chodikov@sparkfun.com>
2024-01-22 14:48:23 -0700

    Updated arduino example folder structure, names, examples
    Generic outline for examples created - setup functionality mostly completed. Folder structure, and names have been adjusted for more examples.

---
+++ examples/Example01_DistanceBasicReadings/Example01_DistanceBasicReadings.ino
@@ -0,0 +1,56 @@
+/*
+  Example 1: Distance Basic Readings
+
+  Using the Acconeer XM125 A121 60GHz Pulsed Coherent Radar Sensor.
+
+  This example shows how operate the XM125 when the device is in Distance Reading Mode.
+  The sensor is initialized, then the distance values will print out to the terminal in 
+  mm. 
+
+  By: Madison Chodikov
+  SparkFun Electronics
+  Date: 2024/1/22
+  SparkFun code, firmware, and software is released under the MIT License.
+	Please see LICENSE.md for further details.
+
+  Hardware Connections:
+  QWIIC --> QWIIC
+
+  Serial.print it out at 115200 baud to serial monitor.
+
+  Feel like supporting our work? Buy a board from SparkFun!
+  https://www.sparkfun.com/products/ - Qwiic XM125 Breakout
+*/
+#include <Arduino.h>
+#include "SparkFun_Qwiic_XM125_Arduino_Library.h"
+
+SfeXM125 radarSensor;
+
+// I2C default address
+uint8_t i2cAddress = SFE_XM125_I2C_ADDRESS;
+
+void setup()
+{
+    // Start serial
+    Serial.begin(115200);
+    Serial.println("XM125 Example 1: Basic Distance Readings");
+    Serial.println("");
+
+    // If begin is successful (0), then start example
+    if(radarSensor.begin(i2cAddress, Wire) == 1)
+    {
+        Serial.println("Begin");
+    }
+    else // Otherwise, infinite loop
+    {
+        Serial.println("Device failed to setup - Freezing code.");
+        while(1); // Runs forever
+    }
+}
+
+void loop()
+{
+    // Request Distance Data from the device 
+
+    delay(100);
+}
